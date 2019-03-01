2477c4c9bc64b96d51dc7f1ca60a1818fc63bdf6
Austin Diviness <austin.diviness@gmail.com>
2019-02-28 23:03:37 -0700

    created ios app

---
+++ src/ios/TaxiMetrics/TaxiMetricsTests/TaxiMetricsTests.swift
@@ -0,0 +1,34 @@
+//
+//  TaxiMetricsTests.swift
+//  TaxiMetricsTests
+//
+//  Created by Austin Diviness on 2/28/2019.
+//  Copyright © 2019 Austin Diviness. All rights reserved.
+//
+
+import XCTest
+@testable import TaxiMetrics
+
+class TaxiMetricsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
